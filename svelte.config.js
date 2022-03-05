d9f096761ef22366b79c0c963cca40bf10878f9a
Jim Hinds <jahbini@icloud.com>
2022-03-05 23:46:01 +0000

    add coffeescript via svelte-add

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
+import coffee from 'vite-plugin-coffee';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			coffeescript: {
+				bare: true
+			}
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
@@ -13,9 +20,17 @@ const config = {
 			server: {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443 : 24678,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+					host: process.env.HMR_HOST
+						? process.env.HMR_HOST.substring('https://'.length)
+						: 'localhost'
 				}
-			}
+			},
+
+			plugins: [
+				coffee({
+					jsx: false
+				})
+			]
 		}
 	}
 };
